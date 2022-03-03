b889c4fa90d309c550ce98bc01f58a716e722dd9
cho-yong-tae <choyongtae@bu.ac.kr>
2022-03-03 19:57:28 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">리액트 프로그래밍 시작</h1>
+      <h2 align='left'>참고 사이트</h2>
+      <p className='site'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      npm&nbsp;&nbsp;&nbsp;repl.it</p>
+      <h2 align='left'>첫 번째 실습</h2>
+    </div>
+  );
+}
+
+export default App;
